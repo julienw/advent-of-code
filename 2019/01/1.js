57df607fe9fa9144cedcabbd29e0807fe5a9ed03
Julien Wajsberg <felash@gmail.com>
2019-12-02 09:13:48 +0100

    Day 1

---
+++ 2019/01/1.js
@@ -0,0 +1,37 @@
+// Pipe the input to this script to get the result
+
+const readline = require('readline');
+
+async function* processLineByLine() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    crlfDelay: Infinity
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  yield* rl;
+}
+
+async function calculateFuel(input) {
+  let sum = 0;
+  for await (const line of input) {
+    const mass = parseInt(line);
+    const fuel = Math.floor(mass / 3) - 2;
+    sum += fuel;
+  }
+
+  return sum;
+}
+
+function printResult(result) {
+  console.log(result);
+}
+
+async function run() {
+  const lineIterator = processLineByLine();
+  const result = await calculateFuel(lineIterator);
+  printResult(result);
+}
+
+run();
---
+++ 2019/01/2.js
@@ -0,0 +1,51 @@
+// Pipe the input to this script to get the result
+
+const readline = require('readline');
+
+async function* processLineByLine() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    crlfDelay: Infinity
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  yield* rl;
+}
+
+function calculateFuelForMass(mass) {
+  const fuel = Math.floor(mass / 3) - 2;
+  return Math.max(0, fuel);
+}
+
+function calculateFuelForModule(mass) {
+  let fuel = calculateFuelForMass(mass);
+  for (let addedFuel = fuel; addedFuel > 0;) {
+    addedFuel = calculateFuelForMass(addedFuel);
+    fuel += addedFuel;
+  }
+  return fuel;
+}
+
+async function calculateFuel(input) {
+  let sum = 0;
+  for await (const line of input) {
+    const mass = parseInt(line);
+    const fuel = calculateFuelForModule(mass);
+    sum += fuel;
+  }
+
+  return sum;
+}
+
+function printResult(result) {
+  console.log(result);
+}
+
+async function run() {
+  const lineIterator = processLineByLine();
+  const result = await calculateFuel(lineIterator);
+  printResult(result);
+}
+
+run();
