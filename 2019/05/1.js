ebb5711901062be6c9b58a48651beb4f99d71c50
Julien Wajsberg <felash@gmail.com>
2019-12-06 09:38:46 +0100

    day 5

---
+++ 2019/05/1.js
@@ -0,0 +1,165 @@
+// @flow
+// Pipe the input to this script to get the result
+
+const readline = require('readline');
+
+async function* processLineByLine() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    crlfDelay: Infinity,
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  yield* rl;
+}
+
+function splitIntoPositions(input) {
+  return input.split(',');
+}
+
+function fetch(positions, index, mode) {
+  return +mode === 0 ? +positions[+index] : +index;
+}
+
+function assertPosition(mode) {
+  if (+mode === 1) {
+    throw new Error(
+      `Expected mode to be 'position', but was 'immediate' instead.`
+    );
+  }
+}
+
+function add(positions, startIndex, strModes) {
+  const paramsSize = 3;
+  const modes = Array.from(strModes.padStart(paramsSize, '0')).reverse();
+
+  const inputIndex1 = +positions[startIndex + 1];
+  const inputIndex2 = +positions[startIndex + 2];
+  const storeIndex = +positions[startIndex + 3];
+
+  const input1 = fetch(positions, inputIndex1, modes[0]);
+  const input2 = fetch(positions, inputIndex2, modes[1]);
+  const result = input1 + input2;
+  //console.log(`Adding ${input1}(${inputIndex1}) to ${input2}(${inputIndex2}) gives ${result}(${storeIndex})`);
+  assertPosition(modes[2]);
+  positions[storeIndex] = result;
+  return paramsSize + 1;
+}
+
+function multiply(positions, startIndex, strModes) {
+  const paramsSize = 3;
+  const modes = Array.from(strModes.padStart(paramsSize, '0')).reverse();
+
+  const inputIndex1 = +positions[startIndex + 1];
+  const inputIndex2 = +positions[startIndex + 2];
+  const storeIndex = +positions[startIndex + 3];
+
+  const input1 = fetch(positions, inputIndex1, modes[0]);
+  const input2 = fetch(positions, inputIndex2, modes[1]);
+  const result = input1 * input2;
+  //console.log(`Multiplying ${input1}(${inputIndex1}) to ${input2}(${inputIndex2}) gives ${result}(${storeIndex})`);
+  assertPosition(modes[2]);
+  positions[storeIndex] = result;
+  return paramsSize + 1;
+}
+
+function input(positions, index, strModes, inputs) {
+  const paramsSize = 1;
+  const modes = Array.from(strModes.padStart(paramsSize, '0')).reverse();
+
+  const storeIndex = +positions[index + 1];
+
+  assertPosition(modes[0]);
+  positions[storeIndex] = inputs.shift();
+  return paramsSize + 1;
+}
+
+function output(positions, index, strModes) {
+  const paramsSize = 1;
+  const modes = Array.from(strModes.padStart(paramsSize, '0')).reverse();
+
+  const inputIndex = +positions[index + 1];
+
+  const input = fetch(positions, inputIndex, modes[0]);
+  console.log(input);
+  return paramsSize + 1;
+}
+
+type OpcodeSize = number;
+type Operation = (
+  positions: Array<string | number>,
+  index: number,
+  modes: string,
+  inputs: Array<string | number>
+) => OpcodeSize;
+
+type Opcode = '1' | '2' | '3' | '4';
+const operations: { [opcode: Opcode]: Operation } = {
+  '1': add,
+  '2': multiply,
+  '3': input,
+  '4': output,
+};
+
+function processOnePosition(state) {
+  const { index, positions, inputs } = state;
+  if (index >= positions.length) {
+    throw new Error(`Oops we don't have anymore input at position ${index}`);
+  }
+
+  const fullOpcode = positions[index];
+  const opcode = +String(fullOpcode).slice(-2);
+  const modes = String(fullOpcode).slice(0, -2);
+  switch (opcode) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+      // $FlowExpectError this works, I know!
+      state.index += operations[String(opcode)](
+        positions,
+        index,
+        modes,
+        inputs
+      );
+      return true;
+    case 99:
+      return false;
+    default:
+      throw new Error(`Invalid opcode ${opcode} at position ${index}`);
+  }
+}
+
+// This is used for debug only
+// eslint-disable-next-line no-unused-vars
+function printFullState(positions) {
+  console.log(positions.join(','));
+}
+
+async function run() {
+  const lineIterator = processLineByLine();
+  const line = (await lineIterator.next()).value;
+  if (!line) {
+    throw new Error('No input!');
+  }
+  const program: string[] = splitIntoPositions(line);
+
+  const positions: Array<string | number> = (program.slice(): any);
+
+  let continueProgram = true;
+
+  const state = {
+    positions,
+    index: 0,
+    inputs: [1],
+  };
+
+  while (continueProgram) {
+    continueProgram = processOnePosition(state);
+  }
+
+  //printFullState(positions);
+}
+
+run();
---
+++ 2019/05/2.js
@@ -0,0 +1,250 @@
+// @flow
+// Pipe the input to this script to get the result
+
+const readline = require('readline');
+const assert = require('assert');
+
+async function* processLineByLine() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    crlfDelay: Infinity,
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  yield* rl;
+}
+
+function splitIntoPositions(input) {
+  return input.split(',');
+}
+
+function fetch(positions, startIndex, modes, paramIndex) {
+  const inputIndex = +positions[startIndex + paramIndex];
+  const mode = modes[paramIndex - 1];
+  return +mode === 0 ? +positions[+inputIndex] : +inputIndex;
+}
+
+function assertPosition(mode) {
+  if (+mode === 1) {
+    throw new Error(
+      `Expected mode to be 'position', but was 'immediate' instead.`
+    );
+  }
+}
+
+function add(state, strModes) {
+  const { positions, index } = state;
+  const paramsCount = 3;
+  const modes = Array.from(strModes.padStart(paramsCount, '0')).reverse();
+
+  let paramIndex = 1;
+  const input1 = fetch(positions, index, modes, paramIndex++);
+  const input2 = fetch(positions, index, modes, paramIndex++);
+  const result = input1 + input2;
+
+  const storeIndex = +positions[index + paramIndex++];
+  assertPosition(modes[2]);
+
+  positions[storeIndex] = result;
+
+  state.index += paramsCount + 1;
+  assert.equal(paramsCount + 1, paramIndex);
+}
+
+function multiply(state, strModes) {
+  const { positions, index } = state;
+  const paramsCount = 3;
+  const modes = Array.from(strModes.padStart(paramsCount, '0')).reverse();
+
+  let paramIndex = 1;
+  const input1 = fetch(positions, index, modes, paramIndex++);
+  const input2 = fetch(positions, index, modes, paramIndex++);
+  const result = input1 * input2;
+
+  const storeIndex = +positions[index + paramIndex++];
+  assertPosition(modes[2]);
+
+  positions[storeIndex] = result;
+
+  state.index += paramsCount + 1;
+  assert.equal(paramsCount + 1, paramIndex);
+}
+
+function input(state, strModes) {
+  const { positions, index, inputs } = state;
+  const paramsCount = 1;
+  const modes = Array.from(strModes.padStart(paramsCount, '0')).reverse();
+
+  let paramIndex = 1;
+  const storeIndex = +positions[index + paramIndex++];
+  assertPosition(modes[0]);
+
+  positions[storeIndex] = inputs.shift();
+
+  state.index += paramsCount + 1;
+  assert.equal(paramsCount + 1, paramIndex);
+}
+
+function output(state, strModes) {
+  const { positions, index } = state;
+  const paramsCount = 1;
+  const modes = Array.from(strModes.padStart(paramsCount, '0')).reverse();
+
+  let paramIndex = 1;
+  const input = fetch(positions, index, modes, paramIndex++);
+  console.log(input);
+
+  state.index += paramsCount + 1;
+  assert.equal(paramsCount + 1, paramIndex);
+}
+
+function jumpIfTrue(state, strModes) {
+  const { positions, index } = state;
+  const paramsCount = 2;
+  const modes = Array.from(strModes.padStart(paramsCount, '0')).reverse();
+
+  let paramIndex = 1;
+  const input1 = fetch(positions, index, modes, paramIndex++);
+  const input2 = fetch(positions, index, modes, paramIndex++);
+
+  if (input1 !== 0) {
+    state.index = input2;
+  } else {
+    state.index += paramsCount + 1;
+  }
+  assert.equal(paramsCount + 1, paramIndex);
+}
+
+function jumpIfFalse(state, strModes) {
+  const { positions, index } = state;
+  const paramsCount = 2;
+  const modes = Array.from(strModes.padStart(paramsCount, '0')).reverse();
+
+  let paramIndex = 1;
+  const input1 = fetch(positions, index, modes, paramIndex++);
+  const input2 = fetch(positions, index, modes, paramIndex++);
+
+  if (input1 === 0) {
+    state.index = input2;
+  } else {
+    state.index += paramsCount + 1;
+  }
+  assert.equal(paramsCount + 1, paramIndex);
+}
+
+function lessThan(state, strModes) {
+  const { positions, index } = state;
+  const paramsCount = 3;
+  const modes = Array.from(strModes.padStart(paramsCount, '0')).reverse();
+
+  let paramIndex = 1;
+  const input1 = fetch(positions, index, modes, paramIndex++);
+  const input2 = fetch(positions, index, modes, paramIndex++);
+
+  const storeIndex = +positions[index + paramIndex++];
+  assertPosition(modes[2]);
+
+  positions[storeIndex] = input1 < input2 ? 1 : 0;
+
+  state.index += paramsCount + 1;
+  assert.equal(paramsCount + 1, paramIndex);
+}
+
+function equals(state, strModes) {
+  const { positions, index } = state;
+  const paramsCount = 3;
+  const modes = Array.from(strModes.padStart(paramsCount, '0')).reverse();
+
+  let paramIndex = 1;
+  const input1 = fetch(positions, index, modes, paramIndex++);
+  const input2 = fetch(positions, index, modes, paramIndex++);
+
+  const storeIndex = +positions[index + paramIndex++];
+  assertPosition(modes[2]);
+
+  positions[storeIndex] = input1 === input2 ? 1 : 0;
+  state.index += paramsCount + 1;
+  assert.equal(paramsCount + 1, paramIndex);
+}
+
+type Operation = (
+  state: {
+    positions: Array<string | number>,
+    index: number,
+    inputs: Array<string | number>,
+  },
+  modes: string
+) => void;
+
+type Opcode = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
+const operations: { [opcode: Opcode]: Operation } = {
+  '1': add,
+  '2': multiply,
+  '3': input,
+  '4': output,
+  '5': jumpIfTrue,
+  '6': jumpIfFalse,
+  '7': lessThan,
+  '8': equals,
+};
+
+function processOnePosition(state) {
+  const { index, positions } = state;
+  if (index >= positions.length) {
+    throw new Error(`Oops we don't have anymore input at position ${index}`);
+  }
+
+  const fullOpcode = positions[index];
+  // This is a bit complex, but this effectively removes any possible leading 0.
+  const opcode = String(+String(fullOpcode).slice(-2));
+  const modes = String(fullOpcode).slice(0, -2);
+  switch (opcode) {
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+      operations[opcode](state, modes);
+      return true;
+    case '99':
+      return false;
+    default:
+      throw new Error(`Invalid opcode ${opcode} at position ${index}`);
+  }
+}
+
+// This is used for debug only
+// eslint-disable-next-line no-unused-vars
+function printFullState(positions) {
+  console.log(positions.join(','));
+}
+
+async function run() {
+  const lineIterator = processLineByLine();
+  const line = (await lineIterator.next()).value;
+  if (!line) {
+    throw new Error('No input!');
+  }
+  const program: string[] = splitIntoPositions(line);
+
+  const positions: Array<string | number> = (program.slice(): any);
+
+  let continueProgram = true;
+
+  const state = {
+    positions,
+    index: 0,
+    inputs: [5],
+  };
+
+  while (continueProgram) {
+    //printFullState(positions);
+    continueProgram = processOnePosition(state);
+  }
+}
+
+run();
