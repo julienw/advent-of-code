9dce6b5f51c49d9cdc0db7f1a9e0791900032188
Julien Wajsberg <felash@gmail.com>
2021-12-21 09:29:39 +0100

    Unfinished Day 20

---
+++ 2021/20/1.js
@@ -0,0 +1,95 @@
+// @flow
+// Pipe the input to this script to get the result
+
+const readline = require('readline');
+
+async function* processLineByLine() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    crlfDelay: Infinity,
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  yield* rl;
+}
+
+function printResult(...args) {
+  console.log(...args);
+}
+
+type Point = {|
+  x: number,
+  y: number,
+|};
+
+function inputToBitstr(input: string): string {
+  return input.replaceAll('#', '1').replaceAll('.', '0');
+}
+
+function enhance(enhancement: string, grid: string[]): string[] {
+  // We need to increase the size of the starting grid first, by adding 2 pixels
+  // on each side.
+
+  const enlargedGrid = [];
+  const emptyLine = '0'.repeat(grid[0].length + 4);
+  enlargedGrid.push(emptyLine, emptyLine);
+  for (let i = 0; i < grid.length; i++) {
+    enlargedGrid.push('00' + grid[i] + '00');
+  }
+  enlargedGrid.push(emptyLine, emptyLine);
+
+  const enhancedGrid = [];
+  for (let y = 1; y < enlargedGrid.length - 1; y++) {
+    const newLine = [];
+    for (let x = 1; x < enlargedGrid[0].length - 1; x++) {
+      const bitStr =
+        enlargedGrid[y - 1].slice(x - 1, x + 2) +
+        enlargedGrid[y].slice(x - 1, x + 2) +
+        enlargedGrid[y + 1].slice(x - 1, x + 2);
+      const number = parseInt(bitStr, 2);
+      const enhancedValue = enhancement[number];
+      newLine.push(enhancedValue);
+    }
+    enhancedGrid.push(newLine.join(''));
+  }
+  return enhancedGrid;
+}
+
+async function run() {
+  const lineIterator = processLineByLine();
+  let enhancement: string | null = null; // 0 or 1
+  let grid = [];
+
+  for await (const line of lineIterator) {
+    if (!enhancement) {
+      enhancement = inputToBitstr(line);
+      continue;
+    }
+
+    if (!line) {
+      // empty line
+      continue;
+    }
+
+    grid.push(inputToBitstr(line));
+  }
+
+  if (!enhancement) {
+    throw new Error('Please feed some input to this script');
+  }
+
+  const enhanceSteps = 2;
+  for (let i = 0; i < enhanceSteps; i++) {
+    grid = enhance(enhancement, grid);
+    console.log(grid.join('\n'), '\n');
+  }
+
+  const strGrid = grid.join('\n');
+  let result = 0;
+  strGrid.replaceAll('1', () => (result++, '1'));
+
+  printResult(result);
+}
+
+run();
